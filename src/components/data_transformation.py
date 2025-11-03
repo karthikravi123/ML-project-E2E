458da1d8bd8a30749280e563e7880716d6230b3b
karthik <geokarthik09@gmail.com>
2025-11-03 16:04:32 +0530

    model trianer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -46,6 +46,7 @@ class DataTransformation:
                 steps=[ 
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
                     
                 ]
             )
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_obj(file_path,obj):
@@ -16,3 +18,25 @@ def save_obj(file_path,obj):
             dill.dump(obj,file_path)
     except:
         pass
+
+
+def evaluate_model(X_train,X_test,y_train,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            ##git the model
+            model.fit(X_train, y_train)
+
+            #then predict
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]= test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
